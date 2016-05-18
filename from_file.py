619f1ffc33a567e764b08dd0132d2f24abd6d796
cqc <1016903103@qq.com>
2016-05-18 18:35:09 +0800

    rm file

--- from_file.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from lib.main import from_file
-
-if __name__ == "__main__":
-    from_file()
--- from_input.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from lib.main import from_input
-
-if __name__ == "__main__":
-    from_input()
